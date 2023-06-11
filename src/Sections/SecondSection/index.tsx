61f10749803a2fcce8394b8b79ee662ae0d9ff1c
qiweikai <304333748@qq.com>
2023-06-11 13:32:09 +0800

    feat: add section part

--- src/Sections/SecondSection/index.tsx
+++ src/Sections/SecondSection/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import cartoonUrl from '../../assets/cartoon.jpg';
 import foodUrl from '../../assets/food.jpg';
 import movieUrl from '../../assets/movie.png';
@@ -7,34 +7,68 @@ import lifeUrl from '../../assets/life.jpg';
 import styles from './style.module.scss';
 import classNames from "classnames";
 
-
 const tabs = [
   {
     key: "cartoon",
-    title: "动画"
+    title: "动画",
+    url: cartoonUrl
   },
   {
     key: "food",
-    title: "美食"
+    title: "美食",
+    url: foodUrl
   },
   {
     key: "movie",
-    title: "电影"
+    title: "电影",
+    url: movieUrl
   },
   {
     key: "life",
-    title: "生活"
+    title: "生活",
+    url: lifeUrl
   }
 ]
 
 const SecondSection: FC = () => {
   const [activeTap, setActiveTab] = useState<string>("cartoon");
+  const [isFixed, setIsFixed] = useState<boolean>(false);
+  const secondSectionRef = useRef<HTMLDivElement>(null);
+  const activate = (key: string) => {
+    const tabContentEl = document.querySelector(`[data-id=${key}]`);
+    if(tabContentEl) {
+      tabContentEl.scrollIntoView({
+        behavior: "smooth"
+      })
+    }
+    setActiveTab(key)
+  }
+  const onScroll = () => {
+    if(secondSectionRef.current) {
+      const { top } = secondSectionRef.current.getBoundingClientRect();
+      setIsFixed(top <= 0);
+      const sectionNodes = secondSectionRef.current.querySelectorAll('section');
+      Array.from(sectionNodes).forEach((sectionEl: HTMLElement) => {
+        const {top} = sectionEl.getBoundingClientRect();
+        const key = sectionEl.getAttribute('data-id') || "";
+        if(top <= 56) {
+          setActiveTab(key)
+        }
+      })
+    }
+  }
+ useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    }
+  },[]) 
   return (
-    <div className={styles.secondSection}>
-      <ul>
+    <div className={styles.secondSection} ref={secondSectionRef}>
+      <ul className={classNames({[styles.isFixed]: isFixed})}>
         {
           tabs.map(tab => (
-            <li key={tab.key} onClick={() => setActiveTab(tab.key)}>
+            <li key={tab.key} onClick={() => activate(tab.key)}>
               <span>{tab.title}</span>
               <span className={classNames(styles.line, {[styles.visible]: tab.key === activeTap})}></span>
             </li>
@@ -43,22 +77,14 @@ const SecondSection: FC = () => {
       </ul>
       
       <div>
-        <section>
-          <h2>动画</h2>
-          <img src={cartoonUrl} alt="Cartoon" />
-        </section>
-        <section>
-          <h2>美食</h2>
-          <img src={foodUrl} alt="food" />
-        </section>
-        <section>
-          <h2>电影</h2>
-          <img src={movieUrl} alt="movie" />
-        </section>
-        <section>
-          <h2>生活</h2>
-          <img src={lifeUrl} alt="Life" />
-        </section>
+        {
+          tabs.map(tab => (
+            <section data-id={tab.key} key={tab.key}>
+              <h2>{tab.key}</h2>
+              <img src={tab.url} alt={tab.key} />
+            </section>
+          ))
+        }
 
       </div>
     </div>
